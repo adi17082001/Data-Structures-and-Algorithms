3028691a8985dcfecb56d69ed78a0d07dfa40963
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2025-01-10 16:17:08 +0530

    Update mergeSort.cpp

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,56 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int>&arr, int low, int mid, int high){
-	vector<int>temp;
-	//low..mid  -> first array
-	//mid+1...high -> second array
-	int left = low;
-	int right = mid+1;
-	while(left <= mid && right <= high){
-		if(arr[left] <= arr[right]){
-			temp.push_back(arr[left]);
-			left++;
-		}
-		else{
-			temp.push_back(arr[right]);
-			right++;
-		}
-	}
-	while(left <= mid){
-		temp.push_back(arr[left]);
-		left++;
-	}
-	while(right <= high){
-		temp.push_back(arr[right]);
-		right++;
-	}
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
 
-	for(int i = low; i <= high; i++){
-		arr[i] = temp[i - low];
-	}
-	
+    //storing elements in the temporary array in a sorted manner//
 
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
 }
 
-void mergeSort(vector<int>&arr, int low, int high){
-	if(low == high) return;
-	int mid = (low + high)/2;
-	mergeSort(arr, low, mid);
-	mergeSort(arr, mid+1, high);
-	merge(arr, low, mid, high);
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
 }
 
+int main() {
 
-int main(){
-	vector<int>arr = {5,4,3,2,1,0};
-	int low = 0; 
-	int high = arr.size() - 1;
-	mergeSort(arr, low, high);
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
 
-	for(auto i: arr){
-		cout<<i<<" ";
-	}
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
 }
-
-// Time complexity for Merge sort: O(NlogN) -> best, average and worst
