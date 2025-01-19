6c631800e79ea8c0e78aff8bfce9f49dfa3826b6
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2025-01-19 09:33:08 +0530

    Create Find_Min_In_Rotated_Sorted_Array.cpp

---
+++ Binary Search/Find_Min_In_Rotated_Sorted_Array.cpp
@@ -0,0 +1,32 @@
+#include<bit/stdc++.h>
+using namespace std;
+
+int findMinInRotatedSortedArray(vector<int>& nums) {
+  int n = nums.size();
+  int low = 0, high = n-1;
+  int ans = INT_MAX;
+  while(low <= high){
+      int mid = (low + high)/2;
+      if(nums[low] <= nums[high]){
+          ans = min(ans, nums[low]);
+          break;
+      }
+      // left half sorted
+      if(nums[low] <= nums[mid]){
+          ans = min(ans, nums[low]);
+          low = mid + 1;
+      }
+      // right half sorted
+      else{
+          ans = min(ans, nums[mid]);
+          high = mid - 1;
+      }
+  }
+  return ans;  
+}
+
+
+int main(){
+  vector<int>arr = {4,5,6,7,0,1,2};
+  cout << findMinInRotatedSortedArray(arr);
+}
