c188018345e2ba23f38c1d337b00f9c5a76c0c42
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2025-01-24 10:04:22 +0530

    Update Min_No_Of_Days_To_Make_M_Bouqets.cpp

--- Binary Search/Min_No_Of_Days_To_Make_M_Bouqets.cpp
+++ Binary Search/Min_No_Of_Days_To_Make_M_Bouqets.cpp
@@ -1,32 +1,58 @@
-bool isPossible(vector<int>&arr, int day, int m, int k){
-	int ct = 0, noOfBouqets = 0;
-	for(int i = 0; i < arr.size(); i++){
-		if(arr[i] <= day){
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int>&bloomDay, int day, int m, int k) {
+	int ct = 0, noOfB = 0;
+	for(int i = 0; i < bloomDay.size(); i++) {
+		if(bloomDay[i] <= day) {
 			ct++;
 		}
-		else{
-			noOfBouqets += (ct / k);
+		else {
+			noOfB += (ct / k);
+			ct = 0;
 		}
 	}
-	noOfBouqets += (ct / k);
-	return noOfBouqets >= m;
+	noOfB += (ct / k);
+	return (noOfB >= m);
 }
 
 
+int minNoOfDaysToMakeMBouqets_brute(vector<int>&bloomDay, int m, int k){
+	int mini = *min_element(bloomDay.begin(), bloomDay.end());
+	int maxi = *max_element(bloomDay.begin(), bloomDay.end());
 
-int minNoOfDaysToMakeMBouquets(vector<int>& bloomDay, int m, int k) {
-    int mini = *min_element(bloomDay.begin(), bloomDay.end());
-    int maxi = *max_element(bloomDay.begin(), bloomDay.end());
+	for(int i = mini; i <= maxi; i++){
+		if(isPossible(bloomDay, i, m, k)){
+			return i;
+		}
+	}
+	return -1;
+}
 
-    int low = mini, high = maxi, ans = -1;
-    while(low <= high){
-        int mid = (low + high)/2;
-        if(isPossible(bloomDay, mid, m, k)){
-            ans = mid;
-            high = mid - 1;
-        }else{
-            low = mid + 1;
-        }
-    }
-    return ans;
+
+int minNoOfDaysToMakeMBouqets(vector<int>&bloomDay, int m, int k){
+	int mini = *min_element(bloomDay.begin(), bloomDay.end());
+	int maxi = *max_element(bloomDay.begin(), bloomDay.end());
+
+	int low = mini, high = maxi, ans = -1;
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(isPossible(bloomDay, mid, m, k)){
+			ans = mid; 
+			high = mid - 1;
+		}else{
+			low = mid + 1;
+		}
+	}
+	return ans;
+}
+
+
+int main() {
+	vector<int> bloomDay = {7,7,7,7,13,11,12,7};
+
+	int m = 2, k = 3;
+	// cout << isPossible(bloomDay, 7, m, k);
+	// cout << minNoOfDaysToMakeMBouqets_brute(bloomDay,m,k);
+	cout << minNoOfDaysToMakeMBouqets(bloomDay, m, k);
 }
