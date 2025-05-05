8ead098f67f263c6343e54b4a8312b2dacb3c239
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2025-05-05 15:32:37 +0530

    Update Print_all_subsequences.cpp

--- Recursion/Print_all_subsequences.cpp
+++ Recursion/Print_all_subsequences.cpp
@@ -1,32 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printF(int ind,vector<int>&ds, int arr[],int n){
-	if(ind==n){
-		for(auto it:ds){
-			cout<<it<<" ";
-		}
-		if(ds.size()==0){  // to print the empty subsequence
-			cout<<"[]";  
-		}
-		cout<<endl;
+void solve(string ip, string op, vector<string>&res){
+	if(ip.length() == 0){
+		// cout << op << " ";
+		res.push_back(op);
 		return;
 	}
-	// take or pick the particular index into the subsequence
-	ds.push_back(arr[ind]);
-	printF(ind+1,ds,arr,n);
-	ds.pop_back();
-
-	// not pick, or not take condition, this element is not added to your subsequence
-	printF(ind+1,ds,arr,n);
+	string op1 = op;
+	string op2 = op;
+	op2.push_back(ip[0]);
+	ip.erase(ip.begin() + 0);
+	solve(ip, op1, res);
+	solve(ip, op2, res);
 }
 
 int main(){
-	int arr[] = {3,1,2};
-	int n= 3;
-	vector<int>ds;
-	printF(0,ds,arr,n);
+	string ip;
+	cin >> ip;
+	vector<string>res;
+	string op = "";
+	solve(ip, op, res);
+	for(auto i: res){
+		cout << i << endl;
+	}
 }
-
-// Time complexity = O(n(2^n)) approx
-// stack space = O(n)
