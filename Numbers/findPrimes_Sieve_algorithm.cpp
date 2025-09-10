9576cdd2992a4372bdb311b7a040cbf72c2ee772
Aditya Bhosale <73772113+adi17082001@users.noreply.github.com>
2025-09-10 08:55:06 +0530

    Update findPrimes_Sieve_algorithm.cpp

--- Numbers/findPrimes_Sieve_algorithm.cpp
+++ Numbers/findPrimes_Sieve_algorithm.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 //const int N = 1e5+10;
 // Sieve Algorithm  -> find primes from 1 to n
+
+int countPrimes(int n) {
+        if(n == 0 || n == 1) return 0;
+        int ct = 0;
+        int primes[n+1];
+        for(int i = 2; i < n; i++){
+            primes[i] = 1;
+        }
+
+        for(int i = 2; i < sqrt(n); i++){
+            if(primes[i] == 1){
+                for(int j = i*i; j < n; j+=i){
+                    primes[j] = 0;
+                }
+            }
+        }
+        for(int i = 2; i < n; i++){
+            if(primes[i] == 1) ct++;
+        }
+        return ct;
+    }
+
 vector<int>Primes(int n){
 	vector<int>res;
 	vector<bool>isPrime(n+1,1);
