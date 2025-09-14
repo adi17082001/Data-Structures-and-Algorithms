115dc8c94950e0649de3b2ec2f92af27ac7b0fc8
Aditya Bhosale <73772113+adi17082001@users.noreply.github.com>
2025-09-14 07:48:55 +0530

    Create Postfix_to_Prefix.cpp

---
+++ Stack_and_Queue/Postfix_to_Prefix.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+string postfixToPrefix(string s){
+  int n = s.size();
+  int i = 0;
+  stack<string> st;
+
+  while (i < n) {
+    // if operand (letter or digit)
+    if ((s[i] >= 'a' && s[i] <= 'z') ||
+        (s[i] >= 'A' && s[i] <= 'Z') ||
+        (s[i] >= '0' && s[i] <= '9')) {
+            
+        st.push(string(1, s[i]));  // convert char to string
+    } 
+    else {
+        // operator: need two operands
+        string t1 = st.top(); st.pop();
+        string t2 = st.top(); st.pop();
+
+        string temp = string(1, s[i]) + t2 + t1;
+        st.push(temp);
+    }
+        
+    i++;
+  }
+  return st.top();
+}
+
+int main() {
+  string s = "ab-de+f*/";
+  cout << postfixToPrefix(s);
+}
