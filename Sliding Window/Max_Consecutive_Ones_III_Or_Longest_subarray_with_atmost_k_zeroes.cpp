390e325318a8439a7a50adc0623f1328030193a0
Aditya Bhosale <73772113+adi17082001@users.noreply.github.com>
2025-09-21 09:59:01 +0530

    Update Max_Consecutive_Ones_III_Or_Longest_subarray_with_atmost_k_zeroes.cpp

--- Sliding Window/Max_Consecutive_Ones_III_Or_Longest_subarray_with_atmost_k_zeroes.cpp
+++ Sliding Window/Max_Consecutive_Ones_III_Or_Longest_subarray_with_atmost_k_zeroes.cpp
@@ -23,9 +23,29 @@ int longestOnes_brute(vector<int>& arr, int k) {
     return maxLen;
 }
 
+int longestOnes_optimal(vector<int>& nums, int k) {
+    int maxLen = 0, l = 0, r = 0, zero = 0;
+    int n = nums.size();
+    while(r < n){
+        if(nums[r] == 0) zero++;
+
+        while(zero > k){
+            if(nums[l] == 0) zero--;
+            l++;
+        }
+        if(zero <= k){
+            int len = r - l + 1;
+            maxLen = max(maxLen, len);
+        }
+        r++;
+    }
+    return maxLen;
+}
+
 
 int main(){
 	vector<int>arr = {1,1,1,0,0,0,1,1,1,1,0};
 	int k = 2;
-	cout << longestOnes_brute(arr, k);
+	// cout << longestOnes_brute(arr, k);
+	cout << longestOnes_optimal(arr, k);
 }
